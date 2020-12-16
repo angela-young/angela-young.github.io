85e0277050b86caeae84ad0510df4ebe9c99fa00
angela-young <60243824+angela-young@users.noreply.github.com>
2020-12-15 21:07:24 -0500

    React Template

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AboutPage = () => (
+    <>
+    <h1>About Me</h1>
+    <p>text test</p>
+    </>
+);
+
+export default AboutPage;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>
+    <h1>Hello World!</h1>
+    <p>text test</p>
+    </>
+);
+
+export default HomePage;
